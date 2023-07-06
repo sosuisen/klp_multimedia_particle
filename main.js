6d3b238d362fd0e70331874fd9df4d6760c1c60e
sosuisen <hidekazu.kubota@gmail.com>
2023-07-06 18:37:27 +0900

    fix: comments

--- main.js
+++ main.js
@@ -24,8 +24,6 @@ const particles = new PIXI.ParticleContainer(maxSprites, {
 
 app.stage.addChild(particles);
 
-// テクスチャのサイズは縦横ともに2のべき乗とするのが最も処理効率がよい。
-// kyoco_trans256x256.pngは縦横256px
 const tx = PIXI.Texture.from('kyoco_trans256x256.png');
 
 /**
@@ -34,27 +32,27 @@ const tx = PIXI.Texture.from('kyoco_trans256x256.png');
 const initSprite = spr => {
   // 中心位置を指定
   spr.anchor.set(0.5);
-  // 左右の開始位置をずらす
+  // 左右の開始位置をずらします
+  // Math.random()は 0以上1未満の値を返します
   spr.x = Math.random() * app.screen.width;
-  // 上下の開始位置をずらす（負の値なので画面外）
+  // 上下の開始位置をずらします（負の値なので画面外）
   spr.y = - Math.random() * app.screen.height;
   // サイズを変更
-  // Math.random()は 0以上1未満の値を返す
   spr.scale.set(Math.random()/2);
   // 角度はラジアンで指定。左右に最大90度
   spr.rotation = Math.PI / (-2.0 + 4.0 * Math.random());
 
-  // 以下は独自プロパティ
-  // speedプロパティを追加して、速度を格納しておく
+  // 以下は独自プロパティ。JavaScriptではオブジェクトに任意のプロパティを追加できます。
+  // speedプロパティを追加して、速度を格納しておきます
   spr.speed = 1 + Math.random() * 3;
-  // orgXプロパティを追加して、元のx座標をコピーしておく
+  // orgXプロパティを追加して、元のx座標をコピーしておきます
   spr.orgX = spr.x;
 
   return spr;
 };
 
 for (let i = 0; i < maxSprites/2; i++){
-  // 同じテクスチャを使いまわす
+  // 同じテクスチャを使いまわします
   const kyoco = PIXI.Sprite.from(tx);
   initSprite(kyoco);
   particles.addChild(kyoco);
